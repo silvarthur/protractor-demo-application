89653584b53abbfd8f64b43de6145da61d5707f2
Arthur Antunes <arthur.antunes@conductor.com.br>
2019-02-07 08:23:20 -0300

    using arrow functions in the test file

--- tests/tests/login-page-spec.js
+++ tests/tests/login-page-spec.js
@@ -1,11 +1,10 @@
 const path = require("path");
+
 const BasePage = require("../pageobjects/BasePage");
 const LoginPage = require("../pageobjects/LoginPage");
 const HomePage = require("../pageobjects/HomePage");
 
-describe("", function() {
-    let EC = protractor.ExpectedConditions;
-
+describe("login tests", () => {
     basePage = new BasePage();
     loginPage = new LoginPage();
     homePage = new HomePage();
@@ -16,18 +15,18 @@ describe("", function() {
     browser.ignoreSynchronization = true;
     browser.manage().timeouts().implicitlyWait(15000)
 
-    beforeEach(async function() {
+    beforeEach(async() => {
         await browser.get("file://" + absolutePathToTheLoginFile);
     });
 
-    it("should login successfully", async function() {
+    it("should login successfully", async() => {
         await loginPage.login("test@test.com", "12345678");
         await basePage.waitElementToBeClickable(homePage.getLogoutButton());
 
         expect(await homePage.getLogoutButton().getText()).toEqual("Logout");
     });
 
-    it("should not login with blank fields", async function() {
+    it("should not login with blank fields", async() => {
         await loginPage.login("", "");
         expect(await basePage.getAlertText()).toEqual("Os campos de e-mail e senha devem estar preenchidos!");
     });
